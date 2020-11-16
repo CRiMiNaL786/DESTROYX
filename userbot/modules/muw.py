6d1b116ab2ccc2e7245f053941906c3f2b2b7329
Sh1vam <70308097+Sh1vam@users.noreply.github.com>
2020-11-16 18:49:08 +0530

    Update muw.py

--- userbot/modules/muw.py
+++ userbot/modules/muw.py
@@ -5,17 +5,12 @@ async def ues(event):
     if event.fwd_from:
         return
     if event.reply_to_msg_id:
-        chat = await event.get_input_chat()
         r_msg = await event.get_reply_message()
-        if r_msg.media:
-            bot_api_file_id = pack_bot_file_id(r_msg.media)
-            await event.edit("Current Chat ID: `{}`\nFrom User ID: `{}`\nBot API File ID: `{}`".format(str(event.chat_id), str(r_msg.from_id), bot_api_file_id))
-        else:
-            await event.edit("Current Chat ID: `{}`\nFrom User ID: `{}`".format(str(event.chat_id), str(r_msg.from_id))) 
-
         try:
-            new_update = await borg.get_entity(int(("{}".format(str(r_msg.from_id))[17:-1])))
-        except ValueError:
+            new_update = await borg.get_entity(int("{}".format(str(r_msg.from_id))[17:-1]))
+        except :
+            new_update = await borg.get_entity("{}".format(str(r_msg.from_id))[17:-1])
+        else:
             new_update = await borg.get_entity('me')
         await event.edit(new_update.stringify())
 @borg.on(admin_cmd(pattern=r"uis ?(.*)"))
