a94a456e68593e7a5a46e379a54d07b3f52f9562
Sh1vam <70308097+Sh1vam@users.noreply.github.com>
2020-11-17 15:42:42 +0530

    Update deploy.py

--- userbot/modules/deploy.py
+++ userbot/modules/deploy.py
@@ -4,4 +4,4 @@ from userbot.utils import admin_cmd
 @bot.on(admin_cmd(pattern=r"deployme"))
 async def source(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        await e.reply("[Click Here to deploy My Bot](https://heroku.com/deploy?template=https://github.com/Sh1vam/javes-2.0/blob/main)")
+        await e.edit("[Click Here to deploy My Bot](https://heroku.com/deploy?template=https://github.com/Sh1vam/javes-2.0/blob/main)")
