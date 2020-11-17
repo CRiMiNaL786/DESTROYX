8d6275ea165970f185b106eecb07cda32f04a618
Sh1vam <70308097+Sh1vam@users.noreply.github.com>
2020-11-18 00:45:50 +0530

    Add files via upload

--- userbot/modules/_inlinebot.py
+++ userbot/modules/_inlinebot.py
@@ -40,7 +40,8 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
                         )
                     ],
                     [Button.url("Join Channel 📃", "t.me/Shivam_Patel"),
-                ],
+                ]
+		   ]
             )
         await event.answer([result] if result else None)
 
