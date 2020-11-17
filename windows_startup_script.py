bf3650b42ff606dccf452b610105fe149fb303b5
Sh1vam <70308097+Sh1vam@users.noreply.github.com>
2020-11-18 00:00:55 +0530

    Add files via upload

---
+++ windows_startup_script.py
@@ -0,0 +1,6 @@
+from telethon import TelegramClient, events
+API_KEY="Type that here"
+API_HASH="Type that here"
+#get it from my.telegram.org
+bot = TelegramClient('userbot',API_KEY,API_HASH)
+bot.start()
