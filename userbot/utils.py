46ad779915426f5ec7116df25849c8cccf7c270f
Sh1vam <70308097+Sh1vam@users.noreply.github.com>
2020-11-18 15:06:49 +0530

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -1,3 +1,14 @@
 from userbot import bot as borg
 from userbot.events import *
 
+async def edit_or_reply(event, text, parse_mode=None, link_preview=None):
+    link_preview = link_preview or False
+    parse_mode = parse_mode or "md"
+    if event.sender_id in Config.SUDO_USERS:
+        reply_to = await event.get_reply_message()
+        if reply_to:
+            return await reply_to.reply(
+                text, parse_mode=parse_mode, link_preview=link_preview
+            )
+        return await event.reply(text, parse_mode=parse_mode, link_preview=link_preview)
+    return await event.edit(text, parse_mode=parse_mode, link_preview=link_preview)
