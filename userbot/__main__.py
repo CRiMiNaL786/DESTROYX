c6be59ba013f3f03d2c0815b4174048118439e4a
Javes786 <73664034+Javes786@users.noreply.github.com>
2020-11-19 23:59:22 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -6,7 +6,7 @@ from importlib import import_module
 
 
 #####################################
-plugin_channel = "@pldhsys"  #this is official plugin channel for javes 2.0 
+plugin_channel = "@j2plugins"  #this is official plugin channel for javes 2.0 
 #####################################
 
 
