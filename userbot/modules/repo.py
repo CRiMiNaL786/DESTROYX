9dadeb7133e1ecbce1d31c20b4de400dd141131c
Sh1vam <70308097+Sh1vam@users.noreply.github.com>
2020-11-19 22:09:12 +0530

    Update repo.py

--- userbot/modules/repo.py
+++ userbot/modules/repo.py
@@ -1,7 +1,7 @@
 from userbot import bot
 from userbot.utils import admin_cmd
 
-@bot.on(admin_cmd(pattern=r"myrepo"))
+@bot.on(admin_cmd(pattern=r"gitrepo"))
 async def source(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
         await e.edit("[Click here](https://github.com/Sh1vam/javes-2.0) to open this Upgraded bot.")
