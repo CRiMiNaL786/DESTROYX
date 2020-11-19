e6aa24862f67cebd98cb33cdae8b2767c574dabb
Javes786 <73664034+Javes786@users.noreply.github.com>
2020-11-19 16:41:39 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -46,7 +46,7 @@ async def a():
      la += 1
      LOGS.info(f" loaded {la}/{f} modules")  
   #os.system("rm userbot/modules/*.py") ; 
-  LOGS.info(f"Sucessfully connected with {o}{o2}{o3}{o4} check it by typing !javes in any client's chat, type  !help for more info.")
+  LOGS.info(f"Sucessfully connected with {o}{o2}{o3}{o4} check it by typing !javes in any client's chat, type  .help for more info.")
   if len(argv) not in (1, 3, 4):
        await javes.disconnect()
   else:
