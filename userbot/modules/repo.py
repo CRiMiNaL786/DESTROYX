5226de1d6600e9bdbf21a2529ae7807286f889c7
Javes786 <73664034+Javes786@users.noreply.github.com>
2020-11-30 12:57:17 +0530

    Delete repo.py

--- userbot/modules/repo.py
+++
@@ -1,7 +0,0 @@
-from userbot import bot
-from userbot.utils import admin_cmd
-
-@bot.on(admin_cmd(pattern=r"getrepo"))
-async def source(e):
-    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        await e.edit("[Click here](https://github.com/Javes786/javes-2.0) to open this Upgraded bot.")
