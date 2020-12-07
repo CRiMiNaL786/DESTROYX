cfb53289edeb4b82b72968db9da364a8fd8bca66
Javes786 <73664034+Javes786@users.noreply.github.com>
2020-12-07 20:34:30 +0530

    Add files via upload

---
+++ userbot/modules/Amaan.py
@@ -0,0 +1,36 @@
+from telethon import events
+import random, re
+from userbot.utils import admin_cmd
+import asyncio 
+
+
+
+@borg.on(admin_cmd("amaan ?(.*)"))
+async def _(event):
+     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
+        await event.edit("Badshah")
+        await asyncio.sleep(0.5)
+        await event.edit("Ki")
+        await asyncio.sleep(0.5)
+        await event.edit("Gali")
+        await asyncio.sleep(0.5)
+        await event.edit("Mai")
+        await asyncio.sleep(0.5)
+        await event.edit("Aake Uska Pta")
+        await asyncio.sleep(0.8)
+        await event.edit("Nhi Puchte")
+        await asyncio.sleep(0.6)
+        await event.edit("Gulamo Ke")
+        await asyncio.sleep(0.6)
+        await event.edit("Jhuke Hue Sar")
+        await asyncio.sleep(0.6)
+        await event.edit("Khud Bae Khud")
+        await asyncio.sleep(0.8)
+        await event.edit("Raasta Dikhaa")
+        await asyncio.sleep(0.7)
+        await event.edit("Dete Hai")
+        await asyncio.sleep(0.6)
+        await event.edit("Tera Baap")
+        await asyncio.sleep(0.8)
+        await event.edit("ATGK @CRiMiNaL786")
+        await asyncio.sleep(0.9)
