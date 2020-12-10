d9e949e7610f9cd8a3c03708002125660b859997
Anmol-dot283 <73867910+Anmol-dot283@users.noreply.github.com>
2020-12-10 22:03:39 +0530

    Update uniborgConfig.py

--- userbot/uniborgConfig.py
+++ userbot/uniborgConfig.py
@@ -124,6 +124,7 @@ if ENV:
         # Google Drive ()
         G_DRIVE_CLIENT_ID = os.environ.get("G_DRIVE_CLIENT_ID", None)
         G_DRIVE_CLIENT_SECRET = os.environ.get("G_DRIVE_CLIENT_SECRET", None)
+        CMD_HNDLR = os.environ.get("CMD_HNDLR", "\!")
         GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", None)
         AUTH_TOKEN_DATA = os.environ.get("AUTH_TOKEN_DATA", None)
         if AUTH_TOKEN_DATA != None:
