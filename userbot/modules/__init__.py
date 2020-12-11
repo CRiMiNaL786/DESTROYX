47ab3360641304e3d445876b137f9515e5b93880
Anmol-dot283 <73867910+Anmol-dot283@users.noreply.github.com>
2020-12-11 14:03:22 +0530

    Update __init__.py

--- userbot/modules/__init__.py
+++ userbot/modules/__init__.py
@@ -1,18 +1,67 @@
+from userbot import topfunc
+from userbot.uniborgConfig import Config
+from userbot.utils import admin_cmd
+from var import Var
 
-from userbot import LOGS
+idgen = topfunc.id_generator
+findnemo = topfunc.stark_finder
+issudousing = Config.SUDO_USERS
+islogokay = Config.PRIVATE_GROUP_ID
+isdbfine = Var.DB_URI
+isherokuokay = Var.HEROKU_APP_NAME
+gdriveisshit = Config.AUTH_TOKEN_DATA
+wttrapi = Config.OPEN_WEATHER_MAP_APPID
+rmbg = Config.REM_BG_API_KEY
+hmmok = Config.LYDIA_API
+currentversion = "4.0"
+telever = "5.0"
 
-def __list_all_modules():
-    from os.path import dirname, basename, isfile
-    import glob
+if issudousing:
+    amiusingsudo = "Active ✅"
+else:
+    amiusingsudo = "Inactive ❌"
 
-    mod_paths = glob.glob(dirname(__file__) + "/*.py")
-    all_modules = [
-        basename(f)[:-3] for f in mod_paths
-        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
-    ]
-    return all_modules
+if islogokay:
+    logchat = "Connected ✅"
+else:
+    logchat = "Dis-Connected ❌"
 
+if isherokuokay:
+    riplife = "Connected ✅"
+else:
+    riplife = "Not Connected ❌"
 
-ALL_MODULES = sorted(__list_all_modules())
-LOGS.info("Loading modules please wait.......")
-__all__ = ALL_MODULES + ["ALL_MODULES"]
+if gdriveisshit:
+    wearenoob = "Active ✅"
+else:
+    wearenoob = "Inactive ❌"
+
+if rmbg:
+    gendu = "Added ✅"
+else:
+    gendu = "Not Added ❌"
+
+if wttrapi:
+    starknoobs = "Added ✅"
+else:
+    starknoobs = "Not Added ❌"
+
+if hmmok:
+    meiko = "Added ✅"
+else:
+    meiko = "Not Added ❌"
+
+if isdbfine:
+    dbstats = "Fine ✅"
+else:
+    dbstats = "Not Fine ❌"
+
+inlinestats = (
+    f"✘ SHOWING Javes 2.0  STATS ✘\n"
+    f"VERSION = {currentversion} \n"
+    f"DATABASE = {dbstats} \n"
+    f"SUDO = {amiusingsudo} \n"
+    f"LOG-CHAT = {logchat} \n"
+    f"HEROKU = {riplife} \n"
+    f"G-DRIVE = {wearenoob}"
+)
