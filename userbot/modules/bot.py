4024a3fa47e8c95d490f4038a0de286ce988064e
Javes786 <73664034+Javes786@users.noreply.github.com>
2021-01-11 21:21:33 +0530

    Update bot.py
    deleted double double plugs

--- userbot/modules/bot.py
+++ userbot/modules/bot.py
@@ -106,68 +106,6 @@ async def _(event):
           
 
 
-@javes.on(rekcah05(pattern=f"history(?: |$)(.*)", allow_sudo=True))
-@javes05(outgoing=True, pattern="^!history(?: |$)(.*)")
-async def _(event):
-  sender = await event.get_sender() ; me = await event.client.get_me()
-  if not sender.id == me.id:
-        rkp = await event.reply("`processing...`")
-  else:
-    	rkp = await event.edit("`processing...`")    
-  try:
-    name = username = [] ; reply_message = await event.get_reply_message() 
-    if not event.reply_to_msg_id or reply_message.media:
-       return await rkp.edit("`reply to a user text message`")
-    chat = "@SangMataInfo_bot"   
-    async with event.client.conversation(chat, timeout=7) as conv:
-          try:     
-              test = conv.wait_event(events.NewMessage(incoming=True,from_users=461843263))
-              await event.client.forward_messages(chat, reply_message)
-              response = await test
-          except YouBlockedUserError: 
-              await rkp.edit("`Please unblock @sangmatainfo_bot and try again`")
-              return
-          if response.text.startswith("Forward"):
-             return await rkp.edit("`Privacy error!`")              
-          if response.text.startswith("🔗"):
-          	return await rkp.edit("`No name/username history for this user`")   
-          if response.text.startswith("Name"):
-              name = response.text
-              await rkp.edit(f"` Got name history Trying to get username history....` ")
-          test = conv.wait_event(events.NewMessage(incoming=True,from_users=461843263))         
-          response = await test
-          if response.text.startswith("Username"):
-               username = response.text
-               await rkp.edit("` Got username history Trying to give full results....` ")
-          return await rkp.edit(f"**User History**\n\n{name}\n\n{username}")
-  except Exception as e:
-        error = str(e)
-        if not error:
-            error = f"No response from {chat}"
-        return await rkp.edit(f"**Error**\n\n{error}")
-          
-@javes.on(rekcah05(pattern=f"ag(?: |$)(.*)", allow_sudo=True))
-@javes05(outgoing=True, pattern="^!ag(?: |$)(.*)")
-async def ag(animu):
-    try:
-        await animu.delete()
-    except:
-    	pass
-    text = animu.pattern_match.group(1)
-    if not text:
-        if animu.is_reply:
-            text = (await animu.get_reply_message()).message
-        else:
-            await rkp.edit("`No text given`")
-            return
-    animus = [20, 32, 33, 40, 41, 42, 58]
-    sticcers = await bot.inline_query(
-        "stickerizerbot", f"#{random.choice(animus)}{(deEmojify(text))}")
-    await sticcers[0].click(animu.chat_id,
-                            reply_to=animu.reply_to_msg_id,
-                            silent=True if animu.is_reply else False,
-                            hide_via=True)
-    
 
 
 @javes.on(rekcah05(pattern=f"info(?: |$)(.*)", allow_sudo=True))
@@ -311,39 +249,6 @@ async def _(event):
 
 
 
-@javes.on(rekcah05(pattern=f"info2(?: |$)(.*)", allow_sudo=True))
-@javes05(outgoing=True, pattern="^!info2(?: |$)(.*)")
-async def _(event):
-  sender = await event.get_sender() ; me = await event.client.get_me()
-  if not sender.id == me.id:
-        rkp = await event.reply("`processing...`")
-  else:
-    	rkp = await event.edit("`processing...`")    
-  try:    
-       reply_message = await event.get_reply_message() 
-       if reply_message:
-           if not reply_message.media:
-       	    return await rkp.edit("`Reply to a photo`")
-       if not reply_message:
-          return await rkp.edit("**Error**\n**Usage** read2 reply to a  photo")
-       chat = "@Rekognition_Bot"   
-       async with event.client.conversation(chat, timeout=15) as conv:
-          try:     
-              test = conv.wait_event(events.NewMessage(incoming=True,from_users=461083923))
-              await event.client.forward_messages(chat, reply_message)
-              response = await test
-              test = conv.wait_event(events.NewMessage(incoming=True,from_users=461083923))
-              response = await test
-          except YouBlockedUserError: 
-              return await rkp.edit("`Please unblock @Rekognition_Bot and try again`")
-          if response.text.startswith("Send"):   
-              return  await rkp.edit("`Failed to read`")
-          return await rkp.edit(response.text)
-  except Exception as e:
-        error = str(e)
-        if not error:
-            error = f"No response from {chat}"
-        return await rkp.edit(f"**Error**\n\n{error}")
           
 
 
@@ -487,69 +392,7 @@ async def _(event):
         return await rkp.edit(f"**Error**\n\n{error}")
           
 
-
-@javes.on(rekcah05(pattern=f"mmf(?: |$)(.*)", allow_sudo=True))
-@javes05(outgoing=True, pattern="^!mmf(?: |$)(.*)")
-async def mim(event):
-  sender = await event.get_sender() ; me = await event.client.get_me()
-  if not sender.id == me.id:
-        rkp = await event.reply("`processing...`")
-  else:
-    	rkp = await event.edit("`processing...`")    
-  try:
-    memeVar = event.pattern_match.group(1)
-    if not event.reply_to_msg_id or not memeVar:
-       return await rkp.edit("`Error: usage mmf 'text on top' ; 'text on bottom' reply to an image/sticker/gif ")
-    reply_message = await event.get_reply_message() 
-    if not reply_message or not reply_message.media:
-       return await rkp.edit("`reply to a image/sticker/gif`")
-    chat = "@MemeAutobot"
-    sender = reply_message.sender
-    file_ext_ns_ion = "@memetime.png"
-    file = await event.client.download_file(reply_message.media)
-    uploaded_gif = None 
-    async with event.client.conversation(chat, timeout=7) as bot_conv:
-          try:
-            await silently_send_message(bot_conv, "/start")            
-            await silently_send_message(bot_conv, memeVar)
-            await event.client.send_file(chat, reply_message.media)
-            response = await bot_conv.get_response()
-          except YouBlockedUserError: 
-              return await event.reply("`Please unblock @MemeAutobot and try again`")
-          if "Okay..." in response.text:            
-            thumb = None
-            if os.path.exists(THUMB_IMAGE_PATH):
-                thumb = THUMB_IMAGE_PATH
-            input_str = event.pattern_match.group(1)
-            if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
-                os.makedirs(TEMP_DOWNLOAD_DIRECTORY)
-            if event.reply_to_msg_id:
-                file_name = "meme.png"
-                reply_message = await event.get_reply_message()
-                to_download_directory = TEMP_DOWNLOAD_DIRECTORY
-                downloaded_file_name = os.path.join(to_download_directory, file_name)
-                downloaded_file_name = await event.client.download_media( reply_message, downloaded_file_name )
-                if os.path.exists(downloaded_file_name):
-                    await event.client.send_file( chat, downloaded_file_name, force_document=False, supports_streaming=False, allow_cache=False, thumb=thumb )
-                    os.remove(downloaded_file_name) 
-            response = await bot_conv.get_response()
-            the_download_directory = TEMP_DOWNLOAD_DIRECTORY
-            files_name = "memes.webp"
-            download_file_name = os.path.join(the_download_directory, files_name)
-            await event.client.download_media( response.media, download_file_name )
-            requires_file_name = TEMP_DOWNLOAD_DIRECTORY + "memes.webp"
-            await event.client.send_file( event.chat_id, requires_file_name, reply_to=event.message.reply_to_msg_id, supports_streaming=False, caption="Memifyed" )
-            return await rkp.delete()           
-          elif not is_message_image(reply_message):            
-            return
-          else: 
-               await rkp.delete()
-               return await event.client.send_file(event.chat_id, response.media, reply_to=event.message.reply_to_msg_id)
-  except Exception as e:
-        error = str(e)
-        if not error:
-            error = f"No response from {chat}"
-        return await rkp.edit(f"**Error**\n\n{error}")
+        
           
 
 @javes.on(rekcah05(pattern=f"ss(?: |$)(.*)", allow_sudo=True))
@@ -589,11 +432,7 @@ async def _(event):
 
 CMD_HELP.update({
     "bot":
-    "!history <reply to a user>\
-\n**Usage**: get user name, username history\
-\n\n!ag <text>\
-\n**Usage**: display given text with anime girl..\
-\n\n!info <reply to a message>\
+"\n\n!info <reply to a message>\
 \n**Usage**: get message, user information  \
 \n\n!lyrics2 <song name>\
 \n**Usage**: for get lyrics   \
@@ -607,14 +446,10 @@ CMD_HELP.update({
 \n**Usage**: convert the img/sticker  \
 \n\n!ss4 <reply to a photo/sticker>\
 \n**Usage**: convert the img/sticker  \
-\n\n!info2 <reply to a photo>\
-\n**Usage**: get information about given photo  \
 \n\n!mask <reply to a photo/sticker>\
 \n**Usage**: save your sticker/photo from covid-19  \
 \n\n!ushort <link>\
 \n**Usage**: shorted the url  \
-\n\n!mmf <texttop ; textbottom> <reply to a photo/sticker>\
-\n**Usage**: mmfying the img  \
 \n\n **All Commands support sudo** \
 "
 })
