665a997f540d0185bd830ab46156cc02a14e5960
Amaan The Gamer King <73664034+Javes786@users.noreply.github.com>
2021-03-03 13:47:48 +0530

    Update gprod.py

--- userbot/modules/gprod.py
+++ userbot/modules/gprod.py
@@ -162,6 +162,7 @@ CMD_HELP.update({
 \n**Usage**: Forcefully Adds the particular person in groups in which you are admin and will promote his/her  \
 \n\n.gdemote <username> / <userid> / <reply to a user>\
 \n**Usage**: Demotes the user in which you and he/she is comman and if you are admin  \ 
-\n\n**Commands Doesn't Require Sudo**\
+\n**Commands Doesn't Require Sudo**\
 "
-})
+}
+)
