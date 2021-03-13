9b548c31bf5ff8872c5aed7d67251fe63fa61767
Sh1vam <70308097+Sh1vam@users.noreply.github.com>
2021-03-13 23:26:57 +0530

    Update sh1vamrun.py

--- sh1vamrun.py
+++ sh1vamrun.py
@@ -1,2 +1,2 @@
 import os
-os.system("git clone hhttps://github.com/Javes786/javes-2.0 /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && mv userbot/javes_main/extra/apktool /usr/local/bin && mv userbot/javes_main/extra/apktool.jar /usr/local/bin && chmod +x /usr/local/bin/* && python3 -m userbot")
+os.system("git clone https://github.com/Javes786/javes-2.0 /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && mv userbot/javes_main/extra/apktool /usr/local/bin && mv userbot/javes_main/extra/apktool.jar /usr/local/bin && chmod +x /usr/local/bin/* && python3 -m userbot")
