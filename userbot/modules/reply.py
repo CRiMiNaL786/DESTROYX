c3cc9c16a6c4084099e9544bcabe097697c7ca8d
Amaan The Gamer King <73664034+Javes786@users.noreply.github.com>
2021-05-31 11:47:39 +0530

    Update reply.py

--- userbot/modules/reply.py
+++ userbot/modules/reply.py
@@ -1,9 +1,10 @@
-from userbot import tebot,client,bot
+from userbot import tgbot,client,bot
 from userbot.utils import register
 from telethon import events
 from telethon.tl import functions, types
 from telethon.tl.types import Channel, Chat, User
-@tebot.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
+
+@tgbot.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
 async def _(event):
     me = await client.get_me()
     sendr = event.chat_id
@@ -15,18 +16,18 @@ async def _(event):
 async def sh1vam(event):
     try :
         lb,cn=event.text[7:].split(";")
-        await tebot.send_message(entity=int(lb),message=cn)
+        await tgbot.send_message(entity=int(lb),message=cn)
         await event.edit(f"Message sent to [User](tg://user?id={lb})")
 
     except:
         lb =event.text[7:]
         reply = await event.get_reply_message()
-        await tebot.send_message(entity=int(lb),message=reply)
+        await tgbot.send_message(entity=int(lb),message=reply)
         await event.edit(f"Your Message sent to [User](tg://user?id={lb})")
 
         '''else:
         lb =event.text[7:]
         reply = await event.get_reply_message()
-        await tebot.forward_messages(int(lb),reply)
+        await tgbot.forward_messages(int(lb),reply)
         await event.edit(f"Your Message sent to [User](tg://user?id={lb})")
         return'''
