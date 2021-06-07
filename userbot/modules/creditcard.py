8ca43e056b66c0c44af2b4d5cb5fcb314d7117f8
Amaan The Gamer King <73664034+Javes786@users.noreply.github.com>
2021-06-07 15:05:01 +0530

    Update creditcard.py

--- userbot/modules/creditcard.py
+++ userbot/modules/creditcard.py
@@ -10,18 +10,18 @@ import os
 import asyncio
 from telethon import events
 from userbot.events import javes05
-from faker import Faker as gand
+from faker import Faker as badhiya
 from userbot.utils import admin_cmd, register
 from userbot import bot as borg
 from userbot import bot
 borg = bot
+import 
 
 
 
-
-@bot.on(admin_cmd("cc"))
+@borg.on(admin_cmd("cc"))
 async def _events(dark):
-    cyber = gand()
+    cyber = badhiya()
     killer = cyber.name()
     kali = cyber.address()
     alain = cyber.credit_card_full()
