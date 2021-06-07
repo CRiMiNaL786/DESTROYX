e4ce28f430a720fa9f82abd5fe52b49f049329b7
Amaan The Gamer King <73664034+Javes786@users.noreply.github.com>
2021-06-07 11:53:43 +0530

    Update fed.py

--- userbot/modules/fed.py
+++ userbot/modules/fed.py
@@ -1,6 +1,5 @@
-from userbot import bot
+from userbot import bot as borg
 from telethon import events
-borg = bot
 import asyncio
 from userbot import bot as  borg
 from userbot import CMD_HELP
@@ -15,7 +14,7 @@ from userbot import CMD_HELP
 
 @bot.on(events.NewMessage(pattern=".fed"))
 async def myfeds(event):
-  LEGENDX = await event.edit("Wᴇɪᴛ ᴍᴀsᴛᴇʀ ᴄʜᴇᴄᴋɪɴɢ ʏᴏᴜʀ ᴀʟʟ ғᴇᴅs...``")
+  GAAND = await event.edit("Wᴇɪᴛ ᴍᴀsᴛᴇʀ ᴄʜᴇᴄᴋɪɴɢ ʏᴏᴜʀ ᴀʟʟ ғᴇᴅs...``")
   async with borg.conversation("missrose_bot") as rose:
     await rose.send_message("/start")
     await rose.get_response()
@@ -27,4 +26,4 @@ async def myfeds(event):
       pro = await rose.get_response()
       await borg.send_file(event.chat_id, pro, caption='Cʜᴇᴄᴋᴇᴅ ʙʏ JAVES 2.0 ฅ^•ﻌ•^ฅ')
     else:
-      await LEGENDX.edit(pro.text + "\n\nCʜᴇᴄᴋᴇᴅ ʙʏ JAVES 2.0 ฅ^•ﻌ•^ฅ")
+      await GAAND.edit(pro.text + "\n\nCʜᴇᴄᴋᴇᴅ ʙʏ JAVES 2.0 ฅ^•ﻌ•^ฅ")
