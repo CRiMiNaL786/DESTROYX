4f901f715334ac8c2dcaab3973cddf8cfff3799a
Amaan The Gamer King <73664034+Javes786@users.noreply.github.com>
2021-06-07 14:21:55 +0530

    MaR GaYa MaDaRCHoD

---
+++ userbot/modules/creditcard.py
@@ -0,0 +1,25 @@
+# Ported by @CrimiNaL786
+# Original By 
+# @THE_B_LACK_HAT
+# @danish_00
+# Card Generator
+##############################
+
+
+
+from faker import Faker as gand
+from userbot.utils import admin_cmd as devil_cmd
+from userbot import bot as devil
+from userbot import bot
+devil = bot
+
+
+
+
+@devil.on(devil_cmd("cc"))
+async def _devil(dark):
+    cyber = gand()
+    killer = cyber.name()
+    kali = cyber.address()
+    alain = cyber.credit_card_full()
+    await dark.edit(f"ℕ𝕒𝕞𝕖:-\n{killer}\n\n𝔸𝕕𝕕𝕣𝕖𝕤𝕤:-\n{kali}\n\nℂ𝕒𝕣𝕕:-\n{alain}")
