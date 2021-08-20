15bc88e9786a9e853ebdbf036d609513137f3342
Amaan The Gamer King <73664034+Javes786@users.noreply.github.com>
2021-08-20 14:00:10 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -61,10 +61,10 @@ async def a():
        await javes.disconnect()
   else:
        await javes.run_until_disconnected()
-      except Exception:
-     print (Exception)
-  os.execl(sys.executable, sys.executable, *sys.argv)
-  quit()
+ except Exception:
+       print (Exception)
+    os.execl(sys.executable, sys.executable, *sys.argv)
+    quit()
   javes.start()
 
 
