ddca95572c2718360622d102e25710982d9cb149
Amaan The Gamer King <73664034+Javes786@users.noreply.github.com>
2021-08-25 20:58:09 +0530

    Update sh1vamrun.py

--- sh1vamrun.py
+++ sh1vamrun.py
@@ -1,5 +1,5 @@
 import os
 
 os.system(
-    "git clone https://github.com/Javes786/javes-2.0 /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && mv userbot/javes_main/extra/apktool /usr/local/bin && mv userbot/javes_main/extra/apktool.jar /usr/local/bin && chmod +x /usr/local/bin/* && python3 -m userbot"
+    "git clone https://github.com/CRIMINAL786/DESTROYX /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && mv userbot/javes_main/extra/apktool /usr/local/bin && mv userbot/javes_main/extra/apktool.jar /usr/local/bin && chmod +x /usr/local/bin/* && python3 -m userbot"
 )
