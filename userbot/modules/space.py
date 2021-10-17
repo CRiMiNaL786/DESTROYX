0a5bce897756c7c938201f253d64a0ce9d00cdb7
Amaan The Gamer King <73664034+Javes786@users.noreply.github.com>
2021-10-17 22:41:49 +0530

    Create space.py

---
+++ userbot/modules/space.py
@@ -0,0 +1,11 @@
+from userbot import bot
+from userbot.utils import admin_cmd
+
+@bot.on(admin_cmd(pattern=r"space"))
+async def space(e):
+    await e.edit("ㅤ")
+@bot.on(admin_cmd(pattern=r"blank"))
+async def blank(e):
+    await e.edit("­")
+
+# Ported from shivam's project
